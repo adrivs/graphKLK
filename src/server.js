46b305ae4c592b6c5070c1919bd8b2e3ad07e620
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2019-06-25 02:36:00 +0200

    Add GraphQL's interface

--- src/server.js
+++ src/server.js
@@ -1,4 +1,25 @@
 import express from "express";
+
+// Middleware de express.
+import graphqlHTTP from "express-graphql";
+
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Hello world!"
+  });
+});
+
+const schema = {};
+
+// GraphiQL tool with which you can see the GraphQL interface in this route.
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema: schema
+  })
+);
 
 app.listen(3000, () => console.log("Server on port 3000"));
